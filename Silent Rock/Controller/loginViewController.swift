0fe1b99aca91aa2f01f6bcef57991217f5ee43d3
Michelle Bodart <michelle.bodart@gmail.com>
2022-01-24 16:49:02 -0800

    adds login and register screens

---
+++ Silent Rock/Controller/loginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  loginViewController.swift
+//  Silent Rock
+//
+//  Created by Michelle Bodart on 1/24/22.
+//
+
+import UIKit
+
+class loginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //do initial setup
+    }
+
+}
+
---
+++ Silent Rock/Controller/registerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  registerViewController.swift
+//  Silent Rock
+//
+//  Created by Michelle Bodart on 1/24/22.
+//
+
+import UIKit
+
+class registerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
