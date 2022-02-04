a3a3dd3a9e20a14fa82d30fb4034a44e7305441c
Michelle Bodart <michelle.bodart@gmail.com>
2022-02-04 10:22:42 -0800

    detail LB formatting is better, season filter works

--- Silent Rock/Controller/leaderboardDetailViewController.swift
+++ Silent Rock/Controller/leaderboardDetailViewController.swift
@@ -14,12 +14,14 @@ class leaderboardDetailViewController: UIViewController {
     var tripsList = [Dictionary<String, Any>]()
     var player: Player = Player()
     var detailPlayerUsername: String = ""
+    var season: String = "all"
 
     @IBOutlet weak var table: UITableView!
     @IBOutlet weak var errorMessageLabel: UILabel!
     @IBOutlet weak var refreshButton: BorderButton!
     @IBOutlet weak var filterByStackView: UIStackView!
     @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var filterByButton: UIButton!
     
     
     override func viewDidLoad() {
@@ -39,9 +41,40 @@ class leaderboardDetailViewController: UIViewController {
         refreshButton.isHidden = true
         errorMessageLabel.text = ""
         
+        // set up filter by button
+        self.setUpFilterByMenu()
+        
         // Do any additional setup after loading the view.
     }
 
+    func setUpFilterByMenu() {
+        
+        var result = [
+            UIAction(title: "All time", image: nil, handler: { (_) in
+                self.season = "all"
+                self.table.reloadData()
+            })]
+        
+        for season in Season().getSeasons() {
+            let newSeason = UIAction(title: season, image: nil, handler: { (_) in
+                self.season = season
+                self.table.reloadData()
+            })
+            result.append(newSeason)
+        }
+        
+        var menuItems: [UIAction] {
+            return result
+        }
+        var filterByMenu: UIMenu {
+            return UIMenu(title: "Season:", image: nil, identifier: nil, options: [], children: menuItems)
+        }
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Menu", image: nil, primaryAction: nil, menu: filterByMenu)
+        filterByButton.showsMenuAsPrimaryAction = true
+        filterByButton.changesSelectionAsPrimaryAction = true
+        filterByButton.menu = filterByMenu
+    }
+    
     func apiStuffHidden(bool: Bool) {
         self.table.isHidden = bool
         self.filterByStackView.isHidden = bool
@@ -95,9 +128,9 @@ extension leaderboardDetailViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = table.dequeueReusableCell(withIdentifier: "StatsTableViewCell", for: indexPath) as! StatsTableViewCell
-        let dateTime = self.tripsList[indexPath.row]["date"] as! String
-        let date = String(dateTime.prefix(17))
-        let time = String(dateTime.dropFirst(17))
+        let dateTime = (self.tripsList[indexPath.row]["date"] as! String).split(separator: "!")
+        let date = String(dateTime[0])
+        let time = String(dateTime[1])
         cell.configure(date: date, time: time)
         return cell
     }
@@ -107,7 +140,11 @@ extension leaderboardDetailViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.tripsList.count
+        if self.season == "all" {
+            return self.tripsList.count
+        } else {
+            return (self.tripsList.filter { $0["season"] as! String == self.season }).count
+        }
     }
     
 }
